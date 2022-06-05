facb1b13a6fbe929e57e1bc6914c7ab9cd62507a
Shaurya-Ranjan <71225335+Shaurya-Ranjan@users.noreply.github.com>
2022-06-05 16:57:58 +0530

    Add files via upload

---
+++ src/utils/stripe/stripe.utils.js
@@ -0,0 +1,5 @@
+import { loadStripe } from "@stripe/stripe-js";
+
+export const stripePromise = loadStripe(
+  pk_test_51L7FYVSFn9PREgo3avTCem4lMPqoO0BHgdSzwcBU4wAm52nn3nkfijoS6mtReSklOVGVwgUIgSBAHfwQBGyK74zw00rsWjaIWU
+);
