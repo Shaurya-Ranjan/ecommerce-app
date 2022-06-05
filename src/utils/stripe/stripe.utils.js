746b63d95519dc15fc2d9709f462548a70320b2d
Shaurya-Ranjan <71225335+Shaurya-Ranjan@users.noreply.github.com>
2022-06-05 17:04:05 +0530

    Update stripe.utils.js

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,5 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const stripePromise = loadStripe(
-  pk_test_51L7FYVSFn9PREgo3avTCem4lMPqoO0BHgdSzwcBU4wAm52nn3nkfijoS6mtReSklOVGVwgUIgSBAHfwQBGyK74zw00rsWjaIWU
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 );
